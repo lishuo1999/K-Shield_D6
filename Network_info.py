4a03024b191e78eecb49439951bfcc0ba79844aa
pyomin <69711911+pyomin@users.noreply.github.com>
2022-08-15 12:59:34 +0900

    Update Network_info.py

--- Network_info.py
+++ Network_info.py
@@ -8,7 +8,7 @@ from datetime import datetime
 now = datetime.now()
 
 # 경로 설정
-dir =f"{os.environ['HOME']}/K-Shiled_D6/net"
+dir =f"{os.environ['HOME']}/K-Shield_D6/net"
 
 def make_dir(dir): 
     try:
